91e4749b7b17cb8d2df3edf423272f437747f418
liubinbin <liubinbean@163.com>
2018-12-05 23:35:51 +0800

    add some files

---
+++ src/main/java/cn/liubinbin/pan/utils/ByteUtils.java
@@ -0,0 +1,12 @@
+package main.java.cn.liubinbin.pan.utils;
+
+public class ByteUtils {
+	
+	public static int hashCode(byte[] buf) {
+	    int hash = 1;
+	    for (int i = 0; i < buf.length; i++) {
+	      hash = (31 * hash) + (int) buf[i];
+	    }
+	    return hash;
+	 }
+}
---
+++ src/test/java/cn/liubinbin/pan/experiment/cache/KeyTest.java
@@ -0,0 +1,32 @@
+package test.java.cn.liubinbin.pan.experiment.cache;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.java.cn.liubinbin.pan.experiment.cache.Key;
+
+public class KeyTest {
+
+	@Test
+	public void testHashCode(){
+		byte[] keyByteArray = { 'k', 'e', 'y'};
+		byte[] key1ByteArray = { 'k', 'e', 'y', '1'};
+		Key key1 = new Key(keyByteArray);
+		Key key2 = new Key(keyByteArray);
+		Key key3 = new Key(key1ByteArray);
+		assertEquals(key1.hashCode(), key2.hashCode());
+		assertNotEquals(key1.hashCode(), key3.hashCode());
+	}
+	
+	@Test
+	public void testEquals() {
+		byte[] keyByteArray = { 'k', 'e', 'y'};
+		byte[] key1ByteArray = { 'k', 'e', 'y', '1'};
+		Key key1 = new Key(keyByteArray);
+		Key key2 = new Key(keyByteArray);
+		Key key3 = new Key(key1ByteArray);
+		assertTrue(key1.equals(key2));
+		assertFalse(key1.equals(key3));
+	}
+}
