6a90f9258fb4f5f400375760fa851fb7b2be8691
liubinbin <liubinbean@163.com>
2018-12-24 00:31:43 +0800

    1. seqQueueSize be threadCount. 2. add test result about merge force to onEvent.

--- src/main/java/cn/liubinbin/pan/experiment/log/v3/EntryHandler.java
+++ src/main/java/cn/liubinbin/pan/experiment/log/v3/EntryHandler.java
@@ -2,6 +2,7 @@ package main.java.cn.liubinbin.pan.experiment.log.v3;
 
 import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -24,14 +25,14 @@ public class EntryHandler implements EventHandler<Entry> {
 		this.fileChannel = fileChannel;
 		this.flusher = new Flusher();
 		this.batchOfSync = batchOfSync;
-		this.seqQueue = new LinkedBlockingQueue<SyncMark>(seqQueueSize);
+		this.seqQueue = new ArrayBlockingQueue<SyncMark>(seqQueueSize);
 		new Thread(this.flusher).start();
 	}
 	
 	@Override
 	public void onEvent(Entry entry, long sequence, boolean endOfBatch) throws IOException, InterruptedException {
-		long startTime = System.nanoTime();
 		fileChannel.write(entry.getByteBuffer());
+		long startTime = System.nanoTime();
 		seqQueue.put(entry.getSyncMark());
 //		System.out.println("sequence: " + sequence + " use time: " + (System.nanoTime() - startTime));
 //		System.out.println("On event entry: entry.sequence: " + entry.getSequence() + " " + sequence);
@@ -45,14 +46,16 @@ public class EntryHandler implements EventHandler<Entry> {
 			int batchIdx = 0;
 			while (true) {
 				try {
+					long startTime = System.nanoTime();
 					syncMark = seqQueue.take();
 					batchIdx = (++batchIdx) % batchOfSync;
 					if (batchOfSync == 1 || batchIdx == 0) { 
 						fileChannel.force(true);
+					} else {
 					}
 					syncMark.done();
 //					System.out.println(" Flusher batchIdx: " + batchIdx + " batchOfSync: " + batchOfSync + " syncMark: " + syncMark.getThreadName() + " sequence: " + syncMark.getSequence());
-				} catch (IOException | InterruptedException e) {
+				} catch (InterruptedException | IOException e) {
 					e.printStackTrace();
 				}
 			}
