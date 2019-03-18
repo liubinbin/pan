db237375fa464ee1e584d0adc3979ddd4e6720ce
liubinbin <liubinbean@163.com>
2019-03-18 23:18:16 +0800

    add some usages about concurrentskiplistmap

---
+++ src/main/java/cn/liubinbin/experiment/ccsmap/ConcurrentSkipListMapTest.java
@@ -0,0 +1,24 @@
+package main.java.cn.liubinbin.pan.experiment.csmap;
+
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class ConcurrentSkipListMapTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		ConcurrentSkipListMap<TestKey, String> data = new ConcurrentSkipListMap<TestKey, String>();
+		TestKey abc = new TestKey("abc".getBytes());
+		TestKey abcd = new TestKey("abcd".getBytes());
+//		TestKey abce = new TestKey("abce".getBytes());
+		TestKey abcde = new TestKey("abcde".getBytes());
+		data.put(abc, "abc");
+		data.put(abcd, "abcd");
+		data.put(abcde, "abcde");
+		for (int i = 0; i < 100000000; i++) {
+			data.put(new TestKey((i+"").getBytes()), i+"");
+			System.out.println("i: " + i);
+		}
+		System.out.println("done");
+		Thread.sleep(1000 * 1000);
+	}
+
+}
---
+++ src/main/java/cn/liubinbin/experiment/ccsmap/TestKey.java
@@ -0,0 +1,66 @@
+package main.java.cn.liubinbin.pan.experiment.csmap;
+
+import java.util.Arrays;
+
+import main.java.cn.liubinbin.pan.utils.ByteUtils;
+
+/**
+ *
+ * @author liubinbin TODO key should be put into map
+ */
+public class TestKey implements Comparable<Object> {
+	private byte[] key;
+
+	public TestKey(byte[] key) {
+		this.key = key;
+	}
+
+	public byte[] getKey() {
+		return key;
+	}
+
+	public void setKey(byte[] key) {
+		this.key = key;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if (!(o instanceof TestKey)) {
+			return -1;
+		}
+		
+		byte[] keyToCom = ((TestKey)o).getKey();
+		if (key.length > keyToCom.length) {
+			return 1;
+		} else if (key.length < keyToCom.length) {
+			return -1;
+		}
+		// length is equal
+		if (Arrays.equals(key, keyToCom)){
+			return 0;
+		}
+		for (int keyIdx = 0; keyIdx < key.length; keyIdx++) {
+			if (key[keyIdx] > keyToCom[keyIdx]) {
+				return 1;
+			}
+		}
+		return -1;
+	}
+	
+	@Override
+	public int hashCode() {
+		return ByteUtils.hashCode(key);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof TestKey)) {
+			return false;
+		}
+		if (this.hashCode() == ((TestKey) obj).hashCode() && this.compareTo(obj) == 0){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
