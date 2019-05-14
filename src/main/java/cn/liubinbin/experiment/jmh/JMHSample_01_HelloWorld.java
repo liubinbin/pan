7fd0792eec1d896f73af95fe435a2506444c7ef7
liubinbin <liubinbean@163.com>
2019-05-14 16:56:40 +0800

    jmh example

---
+++ src/main/java/cn/liubinbin/experiment/jmh/JMHSample_01_HelloWorld.java
@@ -0,0 +1,68 @@
+package cn.liubinbin.experiment.jmh;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by bin on 2019/5/14.
+ * ref https://sq.163yun.com/blog/article/179671960481783808
+ */
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+public class JMHSample_01_HelloWorld {
+
+    static class Demo {
+        int id;
+        String name;
+        public Demo(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    static List<Demo> demoList;
+    static {
+        demoList = new ArrayList<>();
+        for (int i = 0; i < 10000; i ++) {
+            demoList.add(new Demo(i, "test"));
+        }
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void testHashMapWithoutSize() {
+        Map map = new HashMap();
+        for (Demo demo : demoList) {
+            map.put(demo.id, demo.name);
+        }
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void testHashMap() {
+        Map map = new HashMap((int)(demoList.size() / 0.75f) + 1);
+        for (Demo demo : demoList) {
+            map.put(demo.id, demo.name);
+        }
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(JMHSample_01_HelloWorld.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+}
