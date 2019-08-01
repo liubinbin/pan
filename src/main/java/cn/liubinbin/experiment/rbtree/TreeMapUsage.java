f31c14125477de260d7bbb3f69bd30d0fbc11012
liubinbin <liubinbean@163.com>
2019-08-01 20:28:28 +0800

    add BinTreeMap.java copyed from jdk src

---
+++ src/main/java/cn/liubinbin/experiment/rbtree/TreeMapUsage.java
@@ -0,0 +1,29 @@
+package cn.liubinbin.experiment.rbtree;
+
+import java.util.Random;
+
+/**
+ *
+ * @author liubinbin
+ *
+ */
+public class TreeMapUsage {
+
+	public static void main(String[] args) {
+		BinTreeMap<Integer, String> treeMap = new BinTreeMap<Integer, String>();
+		Random random = new Random();
+		int tempNum = 0;
+		for (int i = 0; i < 10; i++) {
+			tempNum = random.nextInt(100);
+			treeMap.put(tempNum, "value" + tempNum);
+		}
+        System.out.println(treeMap.size());
+        System.out.println(treeMap.containsKey(1));
+        System.out.println(treeMap.get(1));
+        treeMap.remove(2);
+        System.out.println(treeMap.toString());
+        System.out.println(treeMap.getFirstEntry());
+        System.out.println(treeMap.getLastEntry());
+	}
+
+}
